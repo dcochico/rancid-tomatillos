c9875b591797d322bbf8d585795ad48c19df91b3
Daniel Cochico <dcochico.20@gmail.com>
2023-07-05 21:50:08 -0700

    Create and write Card files

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './css/Card.css';
+
+const Card = ({ id, poster_path, title, average_rating }) => {
+  return (
+    <div>
+      <img src={poster_path} />
+      <h2>{title}</h2>
+      <h3>{average_rating}</h3>
+    </div>
+  )
+}
+
+export default Card;
