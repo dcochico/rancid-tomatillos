285545f52ccb685662edb9896752b0e148eaf96a
Daniel Cochico <dcochico.20@gmail.com>
2023-07-05 22:36:43 -0700

    Shrink movie card images size

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ import '../css/Card.css';
 const Card = ({ id, poster_path, title, average_rating }) => {
   return (
     <div>
-      <img src={poster_path}/>
+      <img 
+        src={poster_path}
+        className='card-poster-path'
+      />
       <h2>{title}</h2>
       <h3>{average_rating}</h3>
     </div>
