abdc055eb6fd917e71ffea65a861506be86c4a05
Natalie Baker <nathelene76@gmail.com>
2023-07-07 15:30:57 -0600

    Add styling changes to nav and movie display

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import '../css/Nav.css';
+import '../images/avatar.png';
 
 const Nav = () => {
     return (
         <nav className="nav-bar">
             <h1 className="site-header">Rancid Tomatillos</h1>
+            <img src='../images/avatar.png'/>
         </nav>
     )
 }
