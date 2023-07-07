eae8dbf8dc3cfe5878b92bd4ce370753b476a3f0
Natalie Baker <nathelene76@gmail.com>
2023-07-06 18:00:59 -0600

    Add Login POST fn

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -6,6 +6,13 @@ const GetData = type => {
         .then(res => res.json())
 }
 
+const Login = (email, password) => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/login`, {
+        method: 'POST',
+        body: JSON.stringify({ email: email, password: password }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+}
 
-
-export default GetData
+export default { GetData, Login }
