3f3f012857aab03922e20c09870046fafe0c93e0
Natalie Baker <nathelene76@gmail.com>
2023-07-10 16:54:32 -0600

    Add intercept to before each

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -1,11 +1,19 @@
 describe('template spec', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.intercept('GET', "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+      statusCode: 200,
+      fixture:"stubAllMovies"
+    })
+     .visit('http://localhost:3000')
   })
 
   it('Should display the title of the application on load', () => {
     
     cy.get('header').contains('h1','Rancid Tomatillos')
+  })
+
+  it('Should display a collection on movies when the application is loaded', () => {
+    
   })
 })
