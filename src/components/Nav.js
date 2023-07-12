5bebd562434e9bdf0bce16ee89e234b42bc1e7e7
Natalie Baker <nathelene76@gmail.com>
2023-07-12 10:18:32 -0600

    add navlink to more info button in preview

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,8 @@
 import '../css/Nav.css';
 import '../images/avatar.png';
-import { useParams, Link, Outlet } from 'react-router-dom';
+import { useParams, Link, Outlet, NavLink } from 'react-router-dom';
 
-const Nav = ({ id, title, average_rating, release_date, tagline, genres, backdrop_path, preview }) => {
+const Nav = ({ id, title, average_rating, release_date, tagline, genres, backdrop_path, preview, displayFocus }) => {
   let styles = {
     backgroundImage: `url(${backdrop_path})`
   }
@@ -12,7 +12,6 @@ const Nav = ({ id, title, average_rating, release_date, tagline, genres, backdro
         !preview ? 
         <div className="header-div">
           <h1 className='site-header'>Rancid Tomatillos</h1>
-          <img src='../images/avatar.png' />
         </div> :
         <div
           className='nav-preview'
@@ -28,8 +27,12 @@ const Nav = ({ id, title, average_rating, release_date, tagline, genres, backdro
             </div>
             <h2 className='preview-tagline'>{tagline}</h2>
             <h3>{genres[0]}</h3>
+            <NavLink to={`/${id}`}>
+              <button onClick={displayFocus}>More Info</button>
+            </NavLink>
           </div>
         </div>
+        
       }
     </nav>
   );
